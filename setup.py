e854f2cd31cb32defd59b8b098476cfd11e45a5d
Matthew Bonner <91755666+Mkbonner@users.noreply.github.com>
2023-07-31 10:32:31 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='',
+    version='1.0.0',
+    url='',
+    author='Matthew Bonner',
+    author_email='fake_email@hotmail.com',
+    description='Undecided',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'pandas >= 1.1'],
+)
